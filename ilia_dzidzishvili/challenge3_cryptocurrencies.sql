0c1c45aa380d3b903e3c2029ec1162eb1058e2b2
Ilia <ilia@digitalgoodiesart.com>
2022-08-19 20:48:46 +0400

    add challenge3_cryptocurrencies.sql

---
+++ ilia_dzidzishvili/challenge3_cryptocurrencies.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE `challenge3_cryptocurrencies` (
+  `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `symbol` varchar(10) DEFAULT NULL,
+  `price` decimal(20,10) DEFAULT '0.0000000000',
+  `max_supply` bigint(20) DEFAULT NULL,
+  `high` decimal(20,10) DEFAULT '0.0000000000',
+  `logo_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
